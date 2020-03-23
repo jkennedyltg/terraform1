733a1d78ed7e4008cdb94b1ccd259f0ac6211c76
jkennedy <jason.kennedy@peoplefluent.com>
2020-03-22 21:06:36 -0700

    adding the aws modules

--- auth.tf
+++ auth.tf
@@ -7,8 +7,14 @@ data "aws_availability_zones" "available" {
 
 }
 
-output "AZs" {
-	value = data.aws_availability_zones.available.names
-	description = "list of aws zones"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.31.0"
+  # insert the 12 required variables here
 }
 
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "10.0.0"
+  # insert the 4 required variables here
+}

04b138632e6136e3107d382b4309fad23b8b2c55
jkennedy <jason.kennedy@peoplefluent.com>
2020-03-23 21:10:28 -0700

    fix typoe

--- vpc.tf
+++ vpc.tf
@@ -9,11 +9,8 @@ module "vpc" {
   version = "2.31.0"
 	name = "vpc-simple-test1"
 	cidr = "10.10.0.0/16"
-  azs = "us-west-2c"
-	public_subnets = {"10.10.5.0/24"}
-	tags = {
-		"vpc-simple-test1"
-	}
+  azs = ["us-west-2c", "us-west-2d"]
+	public_subnets = ["10.10.5.0/24", "10.10.10.0/24"]
   # insert the 12 required variables here
 }
 
